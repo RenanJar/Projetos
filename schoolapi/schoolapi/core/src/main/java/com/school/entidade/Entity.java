8f3f021d301c807c6710c1b8dc094d3fb1ae5be6
Renan <Renan.Olive.nasc@gmail.com>
2024-04-23 21:32:37 -0300

    feat(Endereco): Adicionando classe de Endereço que fará parte do agregado de Aluno

---
+++ schoolapi/schoolapi/core/src/main/java/com/school/entidade/Entity.java
@@ -0,0 +1,12 @@
+package com.school.entidade;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Entity {
+    protected UUID ID;
+    protected LocalDate dataInclusao;
+    protected LocalDate dataAtualizacao;
+    protected LocalDate dataDelete;
+    protected Boolean isDelete;
+}
