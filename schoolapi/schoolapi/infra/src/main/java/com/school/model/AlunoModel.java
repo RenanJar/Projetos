3212ae0e712336a944760d7a0e309a158fc6e1a1
Renan <Renan.Olive.nasc@gmail.com>
2024-05-09 20:14:48 -0300

    Feature(MapStruct) Adicionando uma nova dependencia para melhorar o mapeamento entre objetos

--- schoolapi/schoolapi/infra/src/main/java/com/school/model/AlunoModel.java
+++ schoolapi/schoolapi/infra/src/main/java/com/school/model/AlunoModel.java
@@ -1,20 +1,11 @@
 package com.school.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
 @Table(name="TBL_ALUNO")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class AlunoModel extends BaseModel{
 
     @Id
@@ -37,4 +28,62 @@ public class AlunoModel extends BaseModel{
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id", referencedColumnName = "enderecoid")
     private EnderecoModel endereco;
+
+    public AlunoModel(LocalDate dataAtualizacao, LocalDate dataInclusao, LocalDate dataDelete, Boolean isDeletado, UUID id, String nome, LocalDate dataNascimento, Long numeroContato, String email, EnderecoModel endereco) {
+        super(dataAtualizacao, dataInclusao, dataDelete, isDeletado);
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.numeroContato = numeroContato;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getNumeroContato() {
+        return numeroContato;
+    }
+
+    public void setNumeroContato(Long numeroContato) {
+        this.numeroContato = numeroContato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public EnderecoModel getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(EnderecoModel endereco) {
+        this.endereco = endereco;
+    }
 }
--- schoolapi/schoolapi/infra/src/main/java/com/school/model/EnderecoModel.java
+++ schoolapi/schoolapi/infra/src/main/java/com/school/model/EnderecoModel.java
@@ -1,25 +1,20 @@
 package com.school.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
 @Table(name="TBL_ENDERECO")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class EnderecoModel extends BaseModel{
 
     @Id
     @Column(name = "enderecoid")
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private UUID ID;
 
     @Column(name = "logradouro")
     private String logradouro;
@@ -45,4 +40,92 @@ public class EnderecoModel extends BaseModel{
     @Column(name = "cep")
     private Long cep;
 
+    public EnderecoModel(LocalDate dataAtualizacao, LocalDate dataInclusao, LocalDate dataDelete, Boolean isDeletado, UUID ID, String logradouro, Long numero, String complemento, String bairro, String cidade, String estado, Long cep, String pais) {
+        super(dataAtualizacao, dataInclusao, dataDelete, isDeletado);
+        this.ID = ID;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.pais = pais;
+    }
+
+    public EnderecoModel() {
+        super();
+    }
+
+    public UUID getID() {
+        return ID;
+    }
+
+    public void setID(UUID ID) {
+        this.ID = ID;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Long getCep() {
+        return cep;
+    }
+
+    public void setCep(Long cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
